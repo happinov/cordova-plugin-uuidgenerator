5eaf047f8f981f0b592283c106a446bc6867f54a
Nicolas Leclerc <nicolas@happinov.fr>
2019-01-17 18:33:03 +0100

    Fixed code for swift 4.2.

--- src/ios/UUIDGeneratorPlugin.swift
+++ src/ios/UUIDGeneratorPlugin.swift
@@ -1,6 +1,6 @@
 
 @objc(UUIDGeneratorPlugin) class UUIDGeneratorPlugin:CDVPlugin {
-	func generate(_ command: CDVInvokedUrlCommand) {
+	@objc func generate(_ command: CDVInvokedUrlCommand) {
         let uuid = UUID().uuidString
         let result = CDVPluginResult(status:CDVCommandStatus_OK, messageAs:uuid)
         commandDelegate.send(result,callbackId:command.callbackId)
